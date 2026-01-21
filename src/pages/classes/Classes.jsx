9298faba7d7692bf3b41bdc1c78a8448a4666f2a
Leon-kings <leonakingeneye2002@gmail.com>
2026-01-21 17:08:03 +0200

    changes on contact management

--- src/pages/classes/Classes.jsx
+++ src/pages/classes/Classes.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/immutability */
 // /* eslint-disable react-hooks/set-state-in-effect */
 // /* eslint-disable no-unused-vars */
 // import React, { useState, useEffect, useRef } from "react";
